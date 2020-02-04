2ab537df4c999c42499aba084e93a7af574c53e4
Doug Kintop <dkintop@hotmail.com>
2020-02-03 19:06:12 -0500

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,17 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
-import NavBar from '../components/NavBar';
-import Home from '../components/Home';
-import Actors from '../components/Actors';
-import Directors from '../components/Directors';
-import Movies from '../components/Movies';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import Home from "../components/Home";
+import Actors from "../components/Actors";
+import Directors from "../components/Directors";
+import Movies from "../components/Movies";
 
-
-const App = (props) => {
+const App = props => {
   return (
     <Router>
-      {/*{code here}*/}
+      <Route path="/"></Route>
     </Router>
   );
 };
 
-export default App
+export default App;
