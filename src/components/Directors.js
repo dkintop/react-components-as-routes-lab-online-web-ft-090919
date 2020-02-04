d9f9b8639bd05aad48a585828a155af12938e146
Doug Kintop <dkintop@hotmail.com>
2020-02-03 19:37:44 -0500

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import { directors } from '../data';
+import React from "react";
+import { directors } from "../data";
 
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map((director, index) => {
+        <div key= {index}>
+          {director.name}</br>
+          <ul>
+            {director.movies.map((movie, index)=>{
+            <li>{movie}</li>
+            })}
+          </ul>
+        </div>;
+      })}
     </div>
   );
-}
+};
 
-export default Directors
+export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { movies } from "../data";
 
-
-function renderMovies(){
-  return movies.forEach(movie=> {
-    <div>{movie.title}</div>
-    <div>{movie.time}</div>
-    
-  })
-}
-
 const Movies = () => {
   return (
     <div>
       <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+        <div key={index}>
+          <h3>Name: {movie.title}</h3>
+          <p>Time: {movie.time}</p>
+          <p>Genres:</p>
+          <ul>
+            {movie.genres.map((genre, index) => (
+              <li key={index}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
